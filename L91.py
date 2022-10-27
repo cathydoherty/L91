0eda80504f797b8876bafdac31cfcc663cb04b51
Cathy Doherty <116841827+cathydoherty@users.noreply.github.com>
2022-10-27 16:39:11 -0400

    Add files via upload
    This function takes a parameter and adds a <br> between each different string in the parameter.

---
+++ L91.py
@@ -0,0 +1,8 @@
+#Cathy Doherty
+def breakify(word):
+    return "<br>".join(word)
+
+
+lines=["Haiku frogs in snow","A limerick came from Nantucket","Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
+
+print(breakify(lines))
